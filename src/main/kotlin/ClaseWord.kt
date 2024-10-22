fce904102d8afc6d71696d512c48ed2e0c5a4a90
zuzu-iceman <julianj1202@gmail.com>
2024-10-22 23:08:21 +0200

    commit final

--- src/main/kotlin/ClaseWord.kt
+++ src/main/kotlin/ClaseWord.kt
@@ -4,32 +4,49 @@ class  ClaseWord{
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val words = (args[0]).split(" ")
-            println(words)
+            val frase = args[0]
+            val longestLength = LargeWord(frase)
+            println("La longuitud de la palabra mas larga es: $longestLength")
+            println(frase)
 
         }
     }
 }
 
-fun launchFrase(words: String): Int {
+fun LargeWord (frase: String): Int {
+    var longestLength  = 0
+    val words = frase.split(" ")
+
+    for (word in words) {
+        if (word.length > longestLength) {
+            longestLength = word.length
+        }
+    }
+    return longestLength
+
+
+}
+
+fun main() {
+    val frase = "Hola buenasaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa tardes"
+
     val className = "ies.julian.ClaseWord"
     val classPath = System.getProperty("java.class.path")
 
+    val parentPid = ProcessHandle.current().pid()
+    println("PID del proceso padre: $parentPid")
+
     val processBuilder = ProcessBuilder(
-        "java", "-cp", classPath, className,
-        words
+        "java", "-cp", classPath, className, frase
     )
 
     val process = processBuilder.start()
+    val childPid = process.pid()
+    println("PID del proceso hijo: $childPid")
+
+    process.waitFor()
+
     val result = process.inputStream.bufferedReader().readText()
-    return result.toInt()
-}
-
-fun LargestWord (word: String) {
-
-}
-
-fun main(args: Array<String>) {
-    val childProcess = launchFrase(args[0])
+    println("Salida del proceso hijo: $result")
 
 }
--- src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-package ies.julian
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-
-}
